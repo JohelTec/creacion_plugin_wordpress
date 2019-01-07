bff75d4fe6f18b2566e363abe5bb4d532e46688f
johel <johel.tec.00@gmail.com>
2019-01-07 14:56:17 -0500

    creacion de nuevo plugin

---
+++ EJEMPLO1/wp-content/plugins/cc_codigo_corto/cc_codigo_corto.php
@@ -0,0 +1,33 @@
+<?php 
+/**
+ * Plugin Name: peru villa codigo corto
+ * Plugin URI: http://www.google.com.pe
+ * Description: Este plugins es para johel practica
+ * Author: Johel Flores inga
+ * Version: 1.0
+ * Author URI: http://www.google.com.pe
+ * License :GPL2
+ * 
+ */
+    add_action('init', 'cc_codigo_corto');  // inicializa
+    
+    if(!function_exists("cc_codigo_corto")){
+        function cc_codigo_corto(){
+            //[cesar][/cesar]
+            add_shortcode('cesar','codigo_corto'); // te da el shordcode para poner
+        }
+
+    }
+    if(!function_exists("codigo_corto")){ // carga una funcion con el shordcode
+        function codigo_corto($args,$content){ // args permiten traer variables desde afuera
+            // return "<hr/> <strong>".$content."</strong>";
+            $num1 = $args["num1"];
+            $num2 = $args["num2"];
+            return "<hr/>num1=".$num1."<br /> num2=".$num2;
+        }
+    }
+
+
+
+
+?>
